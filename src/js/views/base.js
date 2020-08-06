f655397fdcb1969b945beede6e76f7662afc3f09
Mateusz Brzoska <mateuszbrzoska@seznam.cz>
2020-08-06 12:31:54 +0200

    handling loading state - loading spinner added

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -1,5 +1,30 @@
 export const elements = {
-    searchForm: document.querySelector('.search'),
-    searchInput: document.querySelector('.search__field'),
-    resultsList: document.querySelector('.results__list')
-}
+  searchForm: document.querySelector(".search"),
+  searchInput: document.querySelector(".search__field"),
+  resultsList: document.querySelector(".results__list"),
+  searchRes: document.querySelector(".results"),
+};
+
+export const elementStrings = {
+  loader: "loader",
+};
+
+// renderLoader fcn is in base because of reusability
+export const renderLoader = (parent) => {
+  const loader = `
+        <div class="${elementStrings.loader}">
+            <svg>
+                <use href="img/icons.svg#icon-cw"></use>
+            </svg>
+        </div>
+    `;
+  parent.insertAdjacentHTML("afterbegin", loader);
+};
+
+// hide spinner
+export const clearLoader = () => {
+  const loader = document.querySelector(`.${elementStrings.loader}`);
+  if (loader) {
+    loader.parentElement.removeChild(loader);
+  }
+};
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -10,6 +10,23 @@ export const clearResults = () => {
     elements.resultsList.innerHTML = '';
 };
 
+const limitRecipeTitle = (title, limit = 17) => {
+
+    if(title.length > limit) {
+        const newTitle = [];
+        // Split is going to split text by space here and save particular words into an array. Since we have an array, we can use reduce method on it.
+        title.split(' ').reduce((acc,cur) => {
+            if(acc + cur.length <= limit) {
+                newTitle.push(cur); // cur is current word
+            }
+            return acc + cur.length; // updating acc
+        }, 0); // 0 - starting value of acc
+
+        return `${newTitle.join(' ')} ...`;
+    }
+    return title;
+}
+
 const renderRecipe = recipe => {
     const markup = `
     <li>
@@ -18,7 +35,7 @@ const renderRecipe = recipe => {
                 <img src=${recipe.image_url} alt="Test">
             </figure>
             <div class="results__data">
-                <h4 class="results__name">${recipe.title}</h4>
+                <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                 <p class="results__author">${recipe.publisher}</p>
             </div>
         </a>
